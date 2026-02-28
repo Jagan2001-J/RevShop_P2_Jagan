211985ed1a3594f40ad53f2c953ac6723ef3efaa
Palle Jagan Mohan Reddy <pallejaganmohanreddy2001@gmail.com>
2026-02-28 13:19:52 +0530

    Update dto layer and fix errors

--- src/main/java/com/rev/app/dto/RegisterRequestDTO.java
+++ src/main/java/com/rev/app/dto/RegisterRequestDTO.java
@@ -1,4 +1,18 @@
 package com.rev.app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequestDTO {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String role;
 }
